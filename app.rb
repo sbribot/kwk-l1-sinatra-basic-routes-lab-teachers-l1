9cb317a6623f8de4b6d7e9fcfb0b24f10b9528cd
Sara Beth Ribot <sbribot@gmail.com>
2018-06-26 23:19:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,17 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/name' do
-    "My name is Sara Beth Ribot."
-  end
-  
-  get '/hometown' do
-    "My hometown is Cartersville."
-  end
-  
-  get '/favorite-song' do
-    "My favorite song is Fallingwater."
-  end
-  
+
+get '/name' do 
+  "My name is Sara Beth."
+end 
+
 end
